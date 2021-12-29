906ea31b167dc07550759b34debd855b82484c81
DAVID <88878447+migwi81@users.noreply.github.com>
2021-12-29 11:47:07 +0300

    BLGS

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import re
 
-from flask import Flask
+from flask import Flask, url_for, flash
+from flask import request
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
+app.secret_key = "^2gt*hy&ui@h&u45"  # 16
 
 from flask import render_template
 import pymysql
@@ -17,24 +20,87 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/posts")
-def posts():
-    # create your query
-    sql = "SELECT * FROM products"
-    # execute/run
-    # create a cursor used to execute sql
-    cursor = connection.cursor()
-    # now use the cursor to execute your sql.
-    cursor.execute(sql)
-    # check how many rows were returned
-    if cursor.rowcount == 0:
-        return render_template("posts.html", msg="No post found")
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+
+@app.route("/upload", methods=["POST", "GET"])
+def upload():
+    if request.method == "POST":
+        category = request.form["category"]
+        image1 = request.form["image1"]
+        image2 = request.form["image2"]
+        image3 = request.form["image3"]
+        title = request.form["title"]
+        descc = request.form["descc"]
+        content = request.form["content"]
+
+        # to a table for reviews
+        sql = "insert into posts(category, image1,image2,image3,title,descc,content) values(%s,%s,%s,%s,%s,%s,%s)"
+        cursor = connection.cursor()
+        try:
+            cursor.execute(sql, (category, image1, image2, image3, title, descc, content))
+            connection.commit()
+
+            flash("Review Posted Successfully")
+            return render_template("upload.html")
+
+
+
+
+
+        except:
+            return render_template("upload.html")
+
     else:
-        rows = cursor.fetchall()
-        return render_template("posts.html", rows=rows)
-@app.route('/logingoogle',methods=['POST'])
+        flash("Post Posted Successfully")
+        return render_template("upload.html")
+
+
+@app.route('/logingoogle', methods=['POST'])
 def login():
-    return request.form
+    cursor = connection.cursor()
+    email = request.form['email']
+    pic = request.form['pic']
+    username = request.form['userName']
+    uid = request.form['uid']
+    sql = "INSERT INTO `users`( `uuid`, `username`, `profilepic`,`email`) VALUES (%s,%s,%s,%s)"
+    try:
+        cursor.execute(sql, (uid, username, pic, email))
+        connection.commit()
+        return {
+            'state': True,
+            'message': 'success'
+
+        }
+    except:
+        return {
+            'state': False,
+            'message': 'emailEXists'
+
+        }
+@app.route('/localpost', methods=['GET'])
+def localpost():
+    cursor = connection.cursor()
+    sql = "SELECT * FROM `posts`"
+    try:
+        cursor.execute(sql)
+        # connection.commit()
+        data = cursor.fetchAll()
+        return {
+            'state': True,
+            'data': data
+
+        }
+    except:
+        return {
+            'state': False,
+            'data': []
+
+        }
+    # return  request.form    
+    # sql = 
 
 
 if __name__ == "__main__":
